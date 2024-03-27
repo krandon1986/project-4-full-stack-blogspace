27dcdf3e0a4012e75d474f877f932edd88a16182
krandon1986 <krandon1986@yahoo.co.uk>
2024-03-27 02:32:49 +0000

    Update the admin page layout and format

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
 from .models import Post, Comment
+from django_summernote.admin import SummernoteModelAdmin
 
-# Registering Post Model
-admin.site.register(Post)
+# Updating theadmin page's layout and format
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'slug', 'status')
+    search_field = ['title']
+    list_filter = ('status',)
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
 
 # Registering Comment Model
 admin.site.register(Comment)
