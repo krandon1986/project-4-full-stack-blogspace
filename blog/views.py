12cbdf1d40c626d215ed63d7720ab04949ef10ae
krandon1986 <krandon1986@yahoo.co.uk>
2024-03-28 01:21:59 +0000

    corrected error in view.py in the blog folder

--- blog/views.py
+++ blog/views.py
@@ -8,20 +8,20 @@ class PostList(generic.ListView):
     template_name = "blog/index.html"
     paginate_by = 6
 
-    def post_detail(request, slug):
-        """
-        Display an individuel :model: `blog.Post`.
+def post_detail(request, slug):
+    """
+     Display an individual :model: `blog.Post`.
 
-        **Context**
+    **Context**
 
-        ``post``
-            An instance of :model: `blog.Post`.
+    ``post``
+        An instance of :model: `blog.Post`.
         
-        **Template:**
+    **Template:**
 
-        :template: `blog/post_detail.html`
-        """
+    :template: `blog/post_detail.html`
+    """
 
-        queryset = Post.objects.filter(status=1)
-        post = get_object_or_404(queryset, slug=slug)
-        return render(request,"blog/post_detail.html", {"post": post},)
+    queryset = Post.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+    return render(request,"blog/post_detail.html", {"post": post},)
